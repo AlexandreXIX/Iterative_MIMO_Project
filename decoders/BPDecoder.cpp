f8c46749deb4b4fc4d9f112de82590b9ae1762ed
AlexandreXIX <alexandre.p.dixneuf@vanderbilt.edu>
2025-05-06 10:48:11 +0200

    Decoder well advanced, but can't test since I have circular definitions for classes, will have to work on that

--- decoders/BPDecoder.cpp
+++ decoders/BPDecoder.cpp
@@ -2,34 +2,76 @@
 
 #include "Decoder.h"
 
-class BPDecoder : public Decoder {
+class BPDecoder final : public Decoder {
 public:
-  virtual Eigen::MatrixXcd decode(const Eigen::MatrixXcd &Y, const ProblemParameters &myParams, const QAMConstellation &myQAM, const Channel &myChannel) override {
-    // Get problem constants
+  Eigen::MatrixXcd decode(const Eigen::MatrixXcd &Y, const ProblemParameters &myParams, const QAMConstellation &myQAM, const Channel &myChannel) override {
     int N_t = myParams.GetNt();
     int N_r = myParams.GetNr();
-    int M = myParams.GetM();
-    // Get constellation
-    std::unordered_map<int, std::complex<double>> constellation = myQAM.GetMapInt2Complex();
-    // Get channel conditions CSIR
-    MatrixType H = myChannel.GetH();
-    int noise_var = 1; // This is fixed for this simulation
-    // Start decoding
-    std::vector<std::vector<std::vector<double>>> messages;
-    // messages[nt][nr][sym_idx] = message from variable node nt to factor node nr about symbol sym_idx
+    int M = myParams.GetM(); // Constellation size (e.g., 16 for 16-QAM)
+    const int num_iters = 5; // Number of BP iterations
 
-    for (int nt = 0; nt < N_t; nt++) {
-      for (int nr = 0; nr < N_r; nr++) {
-        for (const auto& pair : constellation) {
-          std::complex<double> symbol = pair.second;
-          std::complex<double> estimate = H(nt,nr) * symbol;
-          double prob = exp(-norm(Y(nt) - estimate) / noise_var);
-          messages[nt][nr][pair.first] = prob;
-          // TODO - add missing work here, fix up the loops
-        }
+    // Get QAM mapping and channel
+    std::unordered_map<int, std::complex<double>> constellation = myQAM.GetMapInt2Complex();
+    std::vector<int> symbolIndices;
+    for (const auto& p : constellation) symbolIndices.push_back(p.first);
+    MatrixType H = myChannel.GetH();
+
+    // Precompute inverse noise variance
+    double noise_var = 1.0; // Assumed known and fixed
+    double inv_noise = 1.0 / noise_var;
+
+    // Initialize messages: messages[nt][sym_idx] = log-belief for symbol sym_idx
+    std::vector<std::vector<double>> messages(N_t, std::vector<double>(M, 0.0));
+
+    // Initialize: uniform priors
+    for (int nt = 0; nt < N_t; ++nt) {
+      for (int m = 0; m < M; ++m) {
+        messages[nt][m] = 0.0; // log(1/M) is same for all, can be ignored
       }
     }
-    // TODO - added decoder
+
+    // BP iterations
+    for (int iter = 0; iter < num_iters; ++iter) {
+      std::vector<std::vector<double>> new_messages = messages; // Placeholder for updates
+
+      for (int nt = 0; nt < N_t; ++nt) {
+        for (int m_idx = 0; m_idx < M; ++m_idx) {
+          std::complex<double> symbol = constellation[symbolIndices[m_idx]];
+          double total_likelihood = 0.0;
+
+          for (int nr = 0; nr < N_r; ++nr) {
+            std::complex<double> y = Y(nr, 0); // Assume 1D signal vector
+            std::complex<double> h = H(nr, nt);
+            std::complex<double> interference = 0.0;
+
+            // Estimate interference from all other transmit antennas
+            for (int other_nt = 0; other_nt < N_t; ++other_nt) {
+              if (other_nt == nt) continue;
+              // Use most likely symbol for other_nt (MAP estimate)
+              int max_idx = std::max_element(messages[other_nt].begin(), messages[other_nt].end()) - messages[other_nt].begin();
+              std::complex<double> s_hat = constellation[symbolIndices[max_idx]];
+              interference += H(nr, other_nt) * s_hat;
+            }
+
+            std::complex<double> residual = y - (h * symbol + interference);
+            double prob = exp(-inv_noise * std::norm(residual));
+            total_likelihood += log(prob + 1e-10); // add small epsilon to avoid log(0)
+          }
+
+          new_messages[nt][m_idx] = total_likelihood;
+        }
+      }
+
+      messages = new_messages; // Update
+    }
+
+    // Make hard decisions
+    Eigen::MatrixXcd decoded(N_t, 1);
+    for (int nt = 0; nt < N_t; ++nt) {
+      int max_idx = std::max_element(messages[nt].begin(), messages[nt].end()) - messages[nt].begin();
+      decoded(nt, 0) = constellation[symbolIndices[max_idx]];
+    }
+
     return decoded;
   }
 };
--- tests/decoder_tests.cpp
+++ tests/decoder_tests.cpp
@@ -1,6 +1,7 @@
 // @author Alexandre P.J. Dixneuf
 
 #include "Decoder.h"
+#include "../decoders/BPDecoder.cpp"
 #include <iostream>
 
 // Helper function to print out test results
@@ -11,8 +12,28 @@ std::string OutputTestResults(const bool testResult) {
   return "Failed";
 }
 
+// Just test simple operation
 bool Test1() {
-  return false;
+  constexpr int N_t = 2;
+  constexpr int N_r = 3;
+  constexpr int T = 2;
+  constexpr int M = 16;
+  const ProblemParameters p(N_t, N_r, T, M);
+  MySignal original(&p);
+  QAMConstellation Q(&p);
+  Channel channel(&p);
+  // Get copies of signal
+  MySignal copy = original;
+  // Encode with QAM
+  Q.QAMEncoding(original);
+  Q.QAMEncoding(copy);
+  // Get second channel to propagate
+  channel.ChannelPropagation(copy);
+  BPDecoder decoder;
+  Eigen::MatrixXcd decoded = decoder.decode(copy.CopyData(), p, Q, channel);
+  std::cout << original.CopyData();
+  std::cout << decoded;
+  return true;
 }
 
 bool Test2() {
