edbe7391d958d2bce220ff172b51007b4941c5c2
AlexandreXIX <alexandre.p.dixneuf@vanderbilt.edu>
2025-05-06 10:15:00 +0200

    Fix typo, and add notes to ensure future knowledge of what needs added and what needs changes.

--- decoders/BPDecoder.cpp
+++ decoders/BPDecoder.cpp
@@ -6,7 +6,7 @@ class BPDecoder : public Decoder {
 public:
   virtual Eigen::MatrixXcd decode(const Eigen::MatrixXcd &Y, const ProblemParameters &myParams, const QAMConstellation &myQAM, const Channel &myChannel) override {
     // Get problem constants
-    int Nt = myParams.GetNt();
+    int N_t = myParams.GetNt();
     int N_r = myParams.GetNr();
     int M = myParams.GetM();
     // Get constellation
@@ -18,25 +18,18 @@ public:
     std::vector<std::vector<std::vector<double>>> messages;
     // messages[nt][nr][sym_idx] = message from variable node nt to factor node nr about symbol sym_idx
 
-    for (int nt = 0; nt < Nt; nt++) {
+    for (int nt = 0; nt < N_t; nt++) {
       for (int nr = 0; nr < N_r; nr++) {
         for (const auto& pair : constellation) {
           std::complex<double> symbol = pair.second;
           std::complex<double> estimate = H(nt,nr) * symbol;
           double prob = exp(-norm(Y(nt) - estimate) / noise_var);
           messages[nt][nr][pair.first] = prob;
+          // TODO - add missing work here, fix up the loops
         }
       }
     }
-
-
-
-
-
-
-
-
-
-    return Y; // TODO - added decoder
+    // TODO - added decoder
+    return decoded;
   }
 };
